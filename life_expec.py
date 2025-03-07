e9e331c20f605bcfad98d1074e321c563b25b491
Oladele B <ajayioladeleb@gmail.com>
2025-03-07 05:13:57 +0100

    v1

--- life_expec.py
+++ life_expec.py
@@ -5,7 +5,7 @@ import pandas as pd
 import sklearn
 
 # Load the saved model and scaler
-with open(r"C:\Users\dell\Desktop\Jup\life_expectancy_model.pkl", "rb") as model_file:
+with open(r"C:\Users\dell\Desktop\streamlit apps\life_expectancy_model.pkl", "rb") as model_file:
     model = pickle.load(model_file)
 
 # with open(r"C:\Users\dell\Desktop\Jup\scaler.pkl", "rb") as scaler_file:
